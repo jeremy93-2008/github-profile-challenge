1359561ff46e83102c669a4bfbda19d74883710a
Jeremy Alvarez <jeremy932012@gmail.com>
2023-11-29 01:06:32 +0100

    fix bg image

--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -2,7 +2,7 @@ import { Search } from '../components/Search'
 
 export function Header() {
     return (
-        <section className="github-profile__header flex justify-center bg-hero bg-darkGrayGithub bg-[length:150%_100%] bg-[position:50%] lg:bg-[length:100%] lg:bg-[position:0] bg-no-repeat h-[36vh] lg:h-[19vw]">
+        <section className="github-profile__header flex justify-center bg-hero bg-darkGrayGithub bg-[length:150%_100%] bg-[position:50%] lg:bg-[length:100%] lg:bg-[position:0] bg-no-repeat h-[36vw] lg:h-[19vw]">
             <Search />
         </section>
     )
